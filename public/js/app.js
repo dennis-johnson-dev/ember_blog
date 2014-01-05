a89e9ea4a01572bed5c06701fe6dbffa1a3b8be4
songawee <dennis@songawee.com>
2014-01-04 18:51:43 -0700

    Update newPost route and made the post nested route

--- public/js/app.js
+++ public/js/app.js
@@ -4,27 +4,6 @@ App.Store = DS.Store.extend({
   revision: 11
 });
 
-App.ApplicationAdapter = DS.RESTAdapter.extend({
-  namespace: 'api'
-});
-
-App.Router.map(function() {
-  this.resource('posts', function() {
-  });
-});
-
-App.PostsRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.find('post');
-  }
-});
-
-App.PostsIndexRoute = Ember.Route.extend({
-  setupController: function() {
-    this.controllerFor('posts').set('posts_var', this.modelFor('posts'));
-  }
-});
-
 /* Models */
 App.Post = DS.Model.extend({
   title: DS.attr('string'),
@@ -37,23 +16,62 @@ App.ApplicationSerializer = DS.RESTSerializer.extend({
   }.property()
 });
 
+App.ApplicationAdapter = DS.RESTAdapter.extend({
+  namespace: 'api'
+});
+
+/* Routes */
+App.Router.map(function() {
+  this.resource('posts', function() {
+    this.resource('post', { path: ':post_id' })
+  });
+  this.resource('newPost');
+});
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('post');
+  }
+
+});
+
+App.NewPostRoute = Ember.Route.extend({
+  setupController: function(controller){
+    controller.newRecord();
+  }
+});
+
+/* Controllers */
 App.PostsController = Ember.ArrayController.extend({
+});
+
+App.NewPostController = Ember.ObjectController.extend({
   actions: {
     createPost: function() {
-      if (this.get('title') != undefined && this.get('text') != undefined) {
+      if (!(this.get('title') == undefined || this.get('text') == undefined)){
         var title, text;
+
         title = this.get('title').trim();
         text = this.get('text').trim();
 
-        var post = this.store.createRecord('post', {
+        var post = this.get('store').createRecord('post', {
           title: title,
           text: text
         });
+
+        this.set('title', '');
+        this.set('text', '');
+
         post.save();
+        this.transitionToRoute('posts');
       }
       else {
         console.log('fields not filled in');
       }
     }
-  }
+  },
+
+  newRecord: function() {
+    this.set('content', App.Post);
+  } 
 });
